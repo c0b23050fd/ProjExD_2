e5c13a5b1facc027d5bceae545603ae5dec387d6
yoshiki <c0b23050fd@edu.teu.ac.jp>
2024-06-18 16:10:23 +0900

    演習１ 向き実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,6 +6,7 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 delta = {pg.K_UP:(0,-5), pg.K_DOWN:(0,5), pg.K_LEFT:(-5,0), pg.K_RIGHT:(5,0)}
+accs = [a for a in range(1,11)]
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
@@ -19,24 +20,50 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img_f = pg.transform.flip(kk_img, True, False)
+
+    kk_img_l = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img_r = pg.transform.rotozoom(kk_img_f, 0, 2.0)
+    kk_img_ul = pg.transform.rotozoom(kk_img, -45, 2.0)
+    kk_img_ur = pg.transform.rotozoom(kk_img_f, 45, 2.0)
+    kk_img_dl = pg.transform.rotozoom(kk_img, 45, 2.0)
+    kk_img_dr = pg.transform.rotozoom(kk_img_f, -45, 2.0)
+    kk_img_u = pg.transform.rotozoom(kk_img_f, 90, 2.0)
+    kk_img_d = pg.transform.rotozoom(kk_img, 90, 2.0)
+
+    kk_dct = {(-5,0):kk_img_l,
+              (-5,-5):kk_img_ul,
+              (0,-5):kk_img_u,
+              (5,-5):kk_img_ur,
+              (5,0):kk_img_r,
+              (5,5):kk_img_dr,
+              (0,5):kk_img_d,
+              (-5,5):kk_img_dl}
+    
+    shape = kk_dct[(-5,0)]
+
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
+
     bm_sur = pg.Surface((20,20))
     bm_sur.set_colorkey((0,0,0))
     pg.draw.circle(bm_sur,(0, 0, 255), (10, 10), 10)
     bm_img = bm_sur.get_rect()
-    kk_rct = kk_img.get_rect()
-    kk_rct.center = 900, 400
     bm_img.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
         if kk_rct.colliderect(bm_img):
-            return
+            return #gameover判定
+        
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -47,9 +74,15 @@ def main():
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
 
+        for k,v in kk_dct.items():
+            if tuple(sum_mv) == k:
+                shape = v
+                screen.blit(shape,kk_rct)
+            else:
+                screen.blit(shape,kk_rct)
+
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)
 
         bm_img.move_ip(vx, vy)
         yoko, tate = check_bound(bm_img)
